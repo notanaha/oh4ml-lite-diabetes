a58017b484e517c1cf064f08df22ead297669570
notanaha <notanaha@users.noreply.github.com>
2022-09-06 21:48:50 +0900

    test

--- scripts/train-diabetes.py
+++ scripts/train-diabetes.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license.
+# Licensed under the MIT license. #test2
 
 import argparse, os, json
 import matplotlib.pyplot as plt
